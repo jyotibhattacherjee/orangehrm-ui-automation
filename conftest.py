8186536af160a9271a75a926f473fd4d0a94e04e
Jyoti Bhattacherjee <jyotibhattacherjee7@gmail.com>
2026-03-16 16:55:51 +0530

    Enable headless Chrome for CI execution

--- conftest.py
+++ conftest.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 def browser_instance():
     option = webdriver.ChromeOptions()
     option.add_argument("--incognito")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     option.add_argument("start-maximized")
     driver = webdriver.Chrome(options=option)
     #driver.implicitly_wait(5)
