fa9fcf768bfc0cb5d41c534874b13bf89d81ce2c
Jyoti Bhattacherjee <jyotibhattacherjee7@gmail.com>
2026-03-26 12:00:36 +0530

    feat: add employee functionality with PIM module smoke test

--- Pages/PimPage.py
+++ Pages/PimPage.py
@@ -8,10 +8,30 @@ class PimPage(BasePage):
         super().__init__(driver)
         self.pim_menu = (By.XPATH, "//span[text()='PIM']")
         self.pim_header = (By.XPATH, "//h6[text()='PIM']")
+        self.add_employee_button = (By.XPATH, "//button[text()=' Add ']")
+        self.first_name_input = (By.NAME, "firstName")
+        self.last_name_input = (By.NAME, "lastName")
+        self.save_button = (By.XPATH, "//button[@type='submit']")
+        self.personal_details_header = (By.XPATH, "//h6[text()='Personal Details']")
 
 
     def click_pim_menu(self):
         self.wait.wait_for_clickable(self.pim_menu).click()
 
     def is_pim_header_visible(self):
-        return self.wait.wait_for_visibility(self.pim_header)
+        element = self.wait.wait_for_visibility(self.pim_header)
+        return element.is_displayed()
+
+    def click_add_employee_button(self):
+        self.wait.wait_for_clickable(self.add_employee_button).click()
+
+    def enter_employee_details(self, first_name, last_name):
+        self.wait.wait_for_visibility(self.first_name_input).send_keys(first_name)
+        self.wait.wait_for_visibility(self.last_name_input).send_keys(last_name)
+
+    def click_save_button(self):
+        self.wait.wait_for_clickable(self.save_button).click()
+
+    def is_personal_details_page_displayed(self):
+        element = self.wait.wait_for_visibility(self.personal_details_header)
+        return element.is_displayed()
--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ def browser_instance():
     option = webdriver.ChromeOptions()
     option.add_argument("--incognito")
     option.add_argument("--headless")
+    option.add_argument("--window-size=1920,1080")
     option.add_argument("--no-sandbox")
     option.add_argument("--disable-dev-shm-usage")
     option.add_argument("start-maximized")
