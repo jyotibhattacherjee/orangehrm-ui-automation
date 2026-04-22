117da72110d878cdc0554697b74e4bbebafc2bf8
jyotibhattacherjee <jyotibhattacherjee7@gmail.com>
2026-04-22 13:10:39 +0530

    Implement end-to-end employee workflow with Dynamic IDs and waits (#3)

--- Tests/test_add_employee_smoke.py
+++ Tests/test_add_employee_smoke.py
@@ -17,9 +17,17 @@ class TestAddEmployeeSmoke(BaseTest):
         first_name = "John"
         last_name = "Doe"
         pim_page.enter_employee_details(first_name, last_name)
+        emp_id_before = pim_page.get_employee_id()
+        print("Before Save Employee ID: ", emp_id_before)
 
         pim_page.click_save_button()
         assert pim_page.is_personal_details_page_displayed()
+
+        emp_id_after = pim_page.get_employee_id_after_save()
+        print("After Save Employee ID: ", emp_id_after)
+
+        assert emp_id_before == emp_id_after
+
 
 
 
